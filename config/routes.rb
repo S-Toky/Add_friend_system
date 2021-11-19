a4d8cbffb8087bd30d4e284ad901af7fa410d2bd
S-Toky <stokyr29@gmail.com>
2021-11-19 19:17:00 +0300

    add system add friends

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'pages#index'
   devise_for :users
   resources :users, only: [:index, :show]
+  resources :invitations
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
